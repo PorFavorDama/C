6e4ba16671c98acc94f54bcaf35927f1834f1f54
PorFavorDama <porfavordama@hotmail.com>
2017-10-18 18:36:05 +0300

    Remember!

--- ArrayVariablesAreLikePointers.c
+++ ArrayVariablesAreLikePointers.c
@@ -5,4 +5,5 @@ int main(){
 	printf("Message = %s\n",test);
 	printf("Stored at : %p\n",test);
 	printf("Size (With sentinel char included): %lu\n",sizeof(test));
+	printf("First char : %c\n",*(test));
 }
